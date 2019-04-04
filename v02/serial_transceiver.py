9802ef13b5b198362df7b6383cd5ac19ac04a715
GlebMorgan <glebmorgan@gmail.com>
2019-04-04 11:18:42 +0300

    Moved PelengTransceiver class to serial_transceiver.py Added unit testing Renamed testing.py to manual_testing.py

--- v02/serial_transceiver.py
+++ v02/serial_transceiver.py
@@ -1,5 +1,10 @@
+import struct
+from functools import wraps
+
 import serial
-from utils import alias, Logger
+from checksums import rfc1071
+from utils import alias
+from utils import bytewise, Logger
 
 # TESTME: does log and slog here and in PelengTransceiver actually connected (they should)
 log = Logger("Serial")
@@ -7,6 +12,7 @@ slog = Logger("Packets")
 
 
 # ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ ERRORS ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ #
+
 
 SerialError = alias(serial.serialutil.SerialException)
 
@@ -49,6 +55,7 @@ class BadLrcError(SerialCommunicationError):
 class CommandError(RuntimeError):
     """ Application-level error, indicates invalid command signature / parameters / semantics """
 
+
 # ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ ERRORS ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ #
 
 
@@ -83,3 +90,148 @@ class SerialTransceiver(serial.Serial):
 
     def handleSerialError(self):  # TODO: PelengTransceiver.handleSerialError()
         print(f"{self}.handleSerialError() is NotImplemented")
+
+
+class PelengTransceiver(SerialTransceiver):
+    AUTO_LRC = False
+    RFC_CHECK_DISABLED = True
+    LRC_CHECK_DISABLED = False
+    HEADER_LEN: int = 6  # in bytes
+    STARTBYTE: int = 0x5A
+    MASTER_ADR: int = 0  # should be set in reply to host machine
+
+    def __init__(self, device: int, master: int = MASTER_ADR, **kwargs):
+        super().__init__(**kwargs)
+        self.deviceAddress = device
+        self.masterAddress = master
+
+    class addCRC():
+        """Decorator to sendPacket() method, appending LRC byte to msg"""
+        __slots__ = ('addLRC')
+
+        def __init__(self, addLRC):
+            self.addLRC = addLRC
+
+        def __call__(self, sendPacketFunction):
+            if (not self.addLRC):
+                return sendPacketFunction
+            else:
+                from checksums import lrc
+
+                @wraps(sendPacketFunction)
+                def sendPacketWrapper(wrappee_self, msg, *args, **kwargs):
+                    return sendPacketFunction(wrappee_self, msg + lrc(msg), *args, **kwargs)
+
+                return sendPacketWrapper
+
+    def receivePacket(self) -> bytes:
+        """
+        Reads packet from serial datastream and returns unwrapped data:
+            Reads header and determines the length of payload data
+            Reads payload and wrapper footer (checksum - 2 bytes)
+            Returns payload data if header and data lengths + header and packet checksums are OK. Raises error otherwise
+        If header is not contained in very first bytes of datastream, sequentially reads bytes portions of header length
+        until valid header is found. Raise error otherwise.
+        No extra data is grabbed from datastream after valid packet is successfully read.
+
+        :raises: SerialError, SerialReadTimeoutError, BadDataError, RuntimeError
+        :return: unwrapped high-level data
+        :rtype: bytes
+        """
+
+        bytesReceived = self.read(self.HEADER_LEN)
+
+        # TODO: byteorder here ▼ and everywhere - ?
+        if (len(bytesReceived) == self.HEADER_LEN and bytesReceived[0] == self.STARTBYTE and
+                (self.RFC_CHECK_DISABLED or int.from_bytes(rfc1071(bytesReceived), byteorder='big') == 0)):
+            header = bytesReceived
+            return self.__readData(header)
+        elif (len(bytesReceived) == 0):
+            raise SerialReadTimeoutError("No reply")
+        elif (len(bytesReceived) < self.HEADER_LEN):
+            raise BadDataError(f"Bad header (too small, [{len(bytesReceived)}] out of [{self.HEADER_LEN}])",
+                               dataname="Header", data=bytesReceived)
+        else:
+            if (bytesReceived[0] == self.STARTBYTE):
+                log.warning(f"Bad header checksum (expected '{bytewise(rfc1071(bytesReceived[:-2]))}', "
+                            f"got '{bytewise(bytesReceived[-2:])}'). Header discarded, searching for valid one...",
+                            dataname="Header", data=bytesReceived)
+            else:
+                log.warning(f"Bad data in front of the stream: [{bytewise(bytesReceived)}]. "
+                            f"Searching for valid header...")
+            for i in range(1, 100):  # TODO: limit infinite loop in a better way
+                while True:
+                    startbyteIndex = bytesReceived.find(self.STARTBYTE)
+                    if (startbyteIndex == -1):
+                        if (len(bytesReceived) < self.HEADER_LEN):
+                            raise BadDataError("Failed to find valid header")
+                        bytesReceived = self.read(self.HEADER_LEN)
+                        log.warning(f"Try next {self.HEADER_LEN} bytes: [{bytewise(bytesReceived)}]")
+                    else: break
+                headerReminder = self.read(startbyteIndex)
+                if (len(headerReminder) < startbyteIndex):
+                    raise BadDataError("Bad header", dataname="Header",
+                                       data=bytesReceived[startbyteIndex:] + headerReminder)
+                header = bytesReceived[startbyteIndex:] + headerReminder
+                if (self.RFC_CHECK_DISABLED or int.from_bytes(rfc1071(header), byteorder='big') == 0):
+                    log.info(f"Found valid header at pos {i * self.HEADER_LEN + startbyteIndex}")
+                    return self.__readData(header)
+            else: raise SerialCommunicationError("Cannot find header in datastream, too many attempts...")
+        # TODO: still have unread data at the end of the serial stream sometimes.
+        # scenario that once caused the issue: send 'ms 43 0' without adding a signal value (need to alter the code)
+
+    def __readData(self, header):
+        datalen, zerobyte = self.__parseHeader(header)
+        data = self.read(datalen + 2)  # 2 is wrapper RFC
+        if (len(data) < datalen + 2):
+            raise BadDataError(f"Bad packet (data too small, [{len(data)}] out of [{datalen + 2}])",
+                               dataname="Packet", data=header + data)
+        if (self.RFC_CHECK_DISABLED or int.from_bytes(rfc1071(header + data), byteorder='big') == 0):
+            slog.debug(f"Reply packet [{len(header + data)}]: {bytewise(header + data)}")
+            if (self.in_waiting != 0):
+                log.warning(f"Unread data ({self.in_waiting} bytes) is left in a serial datastream")
+                self.reset_input_buffer()
+                log.info(f"Serial input buffer flushed")
+            return data[:-2] if (not zerobyte) else data[:-3]  # 2 is packet RFC, 1 is zero padding byte
+        else:
+            raise BadRfcError(f"Bad packet checksum (expected '{bytewise(rfc1071(data[:-2]))}', "
+                              f"got '{bytewise(data[-2:])}'). Packet discarded",
+                              dataname="Packet", data=header + data)
+
+    def __parseHeader(self, header):
+        assert (len(header) == self.HEADER_LEN)
+        assert (header[0] == self.STARTBYTE)
+
+        # unpack header (fixed structure - 6 bytes)
+        fields = struct.unpack('< B B H H', header)
+        if (fields[1] != self.masterAddress):
+            raise ValueError(f"Wrong master address (expected '{self.masterAddress}', got '{fields[1]}')")
+        datalen = (fields[2] & 0x0FFF) * 2  # extract size in bytes, not 16-bit words
+        zerobyte = (fields[2] & 1 << 15) >> 15  # extract EVEN flag (b15 in LSB / b7 in MSB)
+        log.debug(f"ZeroByte: {zerobyte == 1}")
+        return datalen, zerobyte
+
+    @addCRC(AUTO_LRC)
+    def sendPacket(self, msg):
+        """
+        Wrap msg and send packet over serial port
+        For DspAssist protocol - if AUTO_LRC is False, it is assumed that LRC byte is already appended to msg
+
+        :param msg: binary payload data
+        :type msg: bytes
+        :return: bytes written count
+        :rtype: int
+        """
+
+        datalen = len(msg)  # get data size in bytes
+        assert (datalen <= 0xFFF)
+        assert (self.deviceAddress <= 0xFF)
+        zerobyte = b'\x00' if (datalen % 2) else b''
+        datalen += len(zerobyte)
+        # below: data_size = datalen//2 ► translate data size in 16-bit words
+        header = struct.pack('< B B H', self.STARTBYTE, self.deviceAddress, (datalen // 2) | (len(zerobyte) << 15))
+        packet = header + rfc1071(header) + msg + zerobyte
+        packetToSend = packet + rfc1071(packet)
+        bytesSentCount = self.write(packetToSend)
+        slog.debug(f"Packet [{len(packetToSend)}]: {bytewise(packetToSend)}")
+        return bytesSentCount
--- v02/testing.py
+++ v02/testing.py
@@ -1,120 +1,89 @@
-import serial
-from utils import bytewise, Logger
+import threading
+import time
+import unittest
 
-from peleng_transceiver import PelengTransceiver, SerialReadTimeoutError
-from serial_transceiver import SerialTransceiver
+from utils import Logger, auto_repr
 
-s = PelengTransceiver(device=12, port='COM10', baudrate=921600, parity='N', timeout=0.5, write_timeout=0.5)
-r = SerialTransceiver(port='COM11', baudrate=921600, parity='N', timeout=0.5, write_timeout=0.5)
+from serial_transceiver import SerialError, PelengTransceiver
 
-# ▼ logging levels aliases
-lvl = {
-    'C': 50,
-    'F': 50,
-    'E': 40,
-    'W': 30,
-    'I': 20,
-    'D': 10,
-    'N': 0,
-}
-
-log = Logger("Testing")
+log = Logger("Tests")
 
 
-def SONY_receiveNative():
-    import devices.sony
-    print('—' * 20 + " NOT FINISHED " + '—' * 20)
-    # return
-
-    d = devices.sony.SONY()
-    msgs = (
-        '88 30 01 FF',
-        '88 30 01 FF FF FF FF FF',
-        '88 30 01 FF FF',
-        '',
-        '88 '*15+'FF',
-        'FF FF 88 30 01 FF FF',
-        '88 30 01 FF',
-    )
-    commands = tuple(map(bytes.fromhex, msgs))
-
-    s.reset_input_buffer()
-    s.reset_output_buffer()
-    r.reset_input_buffer()
-    r.reset_output_buffer()
-
-    devices.sony.log.setLevel(lvl['D'])
-    with s, r:
-        for c in commands:
-            try:
-                s.write(c)
-                reply = d.receiveNative(r)
-                print(f"Command [{len(c)} bytes]: {bytewise(c)}, reply [{len(reply)} bytes]: {bytewise(reply)}")
-                # assert reply == bytes.fromhex('88 30 01 FF')
-            except SerialReadTimeoutError as e: log.showError(e)
+def testRaiseRuntimeError():
+    raise RuntimeError("Test")
 
 
-def SONY_unwrap():
-    import devices.sony
-    d = devices.sony.SONY()
+class DeviceMock:
+    def __init__(self, adr, stopEvent):
+        try: self.tr = PelengTransceiver(device=0, master=adr, port='COM11')
+        except SerialError as e: log.warn(e.args[0])
+        self.tr.close()
+        self.stopEvent = stopEvent
+        self.thread = threading.Thread(name="DeviceMockThread", target=self.commLoop)
 
-    msgs = (
-        '0F 01 88 30 01 FF',
-        '03 00 88 31 01 FF',
-        '01 04 88 32 01 FF',
-        '00 01 88 33 01 FF',
-        'F0 06 88 34 01 FF',
-    )
-    commands = tuple(map(bytes.fromhex, msgs))
+        self.chchreply = bytes.fromhex('01 01 A8 AB AF AA AC AB A3 AA 08')
 
-    with s, r:
-        print(f"Init: device: {d}, CNT_OUT: {d.CNT_OUT}")
-        for i, c in enumerate(commands):
-            payload = d.unwrap(c)
-            print(f"Command [{len(c)} bytes]: {bytewise(c)}, "
-                  f"device: {d}, "
-                  f"payload: {bytewise(payload)}, "
-                  f"CNT_OUT: {d.CNT_OUT}"
-                  )
-            if (i == 1): d.VIDEO_OUT = not d.VIDEO_OUT
+    def start(self):
+        self.thread.start()
 
+    def commLoop(self):
+        with self.tr:
+            print("open?")
+            while not self.stopEvent.is_set():
+                if (self.tr.in_waiting):
+                    print(f"Detected packet, {self.tr.in_waiting} bytes")
+                    command = self.tr.receivePacket()
+                    reply = 'FF FF'
+                    if command.startswith(bytes.fromhex('01 01')):
+                        reply = self.chchreply
+                    self.tr.sendPacket(reply)
 
-def SONY_wrap():
-    import devices.sony
-    d = devices.sony.SONY()
+                elif self.stopEvent.wait(0.1):
+                    return
 
-    actions = (
-        ('', ''),
-        ('FF', ''),
-        ('', 'for par in d: par.value = True'),
-        ('', 'd.CNT_IN = 9'),
-    )
+        self.stopEvent.clear()
 
-    print(f"Init: device: {d}, CNT_OUT: {d.CNT_OUT}")
-    for payload, modificationCode in actions:
-        exec(modificationCode)
-        result = d.wrap(bytes.fromhex(payload))
-        print(f"Data [{len(bytes.fromhex(payload))} bytes]: {payload}, "
-              f"device: {d}, "
-              f"CNT_OUT: {d.CNT_OUT}, "
-              f"result: {bytewise(result, collapseAfter=32)}"
-              )
+    def __repr__(self):
+        return auto_repr(self, f"(port={self.tr.port}{{{'open' if self.tr.is_open else 'closed'}), "
+                               f"adr={self.tr.masterAddress}, running={self.thread.is_alive()}}}")
 
 
-def _testUtf8Char():
-    # code = 0x2982
-    # print(chr(code))
-    for code in range(0x2980, 0x3000):
-        print(chr(code))
+class Test_PelengTransceiver(unittest.TestCase):
+
+    def test_test(self):
+        tr = PelengTransceiver(device=12, port='COM10')
+        stopEvent = threading.Event()
+        dev = DeviceMock(12, stopEvent)
+
+        commands = {
+            'chch': '5A 0C 06 80 9F 73 01 01 A8 AB AF AA AC AB A3 AA 08 00 4E 52',
+            'badrfc': '5A 0C 06 80 9F 73 01 01 A8 AB AF AA AC AB A3 AA 08 00 4E 51',
+            'revrfc': '5A 0C 06 80 9F 73 01 01 A8 AB AF AA AC AB A3 AA 08 00 52 4E',
+            'nozb': '5A 0C 06 80 9F 73 01 01 A8 AB AF AA AC AB A3 AA 08 4E 52',
+        }
+
+        for command, value in commands.items():
+            commands[command] = bytes.fromhex(value)
+
+        def sendCommand(name):
+            tr.sendPacket(commands[name])
+            return tr.receivePacket()
+        try:
+            dev.start()
+            time.sleep(0.1)
+            print(dev)
+
+            self.assertEqual(sendCommand('chch'), bytes.fromhex('01 01 A8 AB AF AA AC AB A3 AA 08'))
+
+        except Exception as e:
+            log.showStackTrace(e)
+        finally:
+            stopEvent.set()
+            dev.thread.join()
+            time.sleep(0.01)
+
+
 
 
 if __name__ == '__main__':
-    functions = tuple(
-            member for name, member in locals().items() if
-            callable(member) and not member.__name__.startswith('_') and member.__module__ == '__main__'
-    )
-    print('Testing functions:')
-    for f in functions: print(f"{' ' * 4}{f}")
-    print()
-
-    functions[-3]()
+    unittest.main()
